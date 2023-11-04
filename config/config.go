4ee5409265db55369df904cee5684363d237c82c
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-11-05 00:30:48 +0800

    feat: load env from docker env

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"github.com/spf13/viper"
 )
 
@@ -10,17 +9,7 @@ var Viper *viper.Viper
 // Load the config file from config/app.env to Viper
 func LoadConfig() *viper.Viper {
 	vp := viper.New()
-	vp.SetConfigName("app")
-	vp.SetConfigType("env")
-	vp.AddConfigPath("config")
 	vp.AutomaticEnv()
-	
-	if err := vp.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", vp.ConfigFileUsed())
-		Viper = vp
-		return vp
-	} else {
-		fmt.Println("Error loading config file:", err)
-		return nil
-	}
+	Viper = vp
+	return vp
 }
